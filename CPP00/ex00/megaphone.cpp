ab61baaea10081c177369b3fdb66fc979ddd3f2d
robdgs <107399627+robdgs@users.noreply.github.com>
2025-07-16 17:48:46 +0200

    ex00

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Megaphone!" << std::endl;
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return 0;
+    }
+
+    for (int i = 1; i < argc; i++)
+    {
+        std::string arg = argv[i];
+        for (size_t j = 0; j < arg.length(); j++)
+        {
+            arg[j] = std::toupper(arg[j]);
+        }
+
+        std::cout << arg;
+    }
+    std::cout << std::endl;
+
     return 0;
 }
+
+
